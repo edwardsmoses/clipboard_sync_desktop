1eaa571a4e230e716ffaf00b3ea6b7a5c8702a4c
Edwards Moses <edwardsmoses3@gmail.com>
2025-10-08 18:03:09 +0100

    Initial Commit

---
+++ clipboard_sync_desktop/clipboard_sync_desktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  clipboard_sync_desktopApp.swift
+//  clipboard_sync_desktop
+//
+//  Created by Edwards Moses on 08/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct clipboard_sync_desktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
